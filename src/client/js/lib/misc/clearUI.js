5461f7f5fa657a5f50253a58e518218bf27c31f2
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-03-19 00:17:55 -0400

    Changes to index html and css styling- Added labels and two different sections for start and end date. Modified scss styling and flex layout for form saved trips and weather section

--- src/client/js/lib/misc/clearUI.js
+++ src/client/js/lib/misc/clearUI.js
@@ -2,6 +2,7 @@ const $INPUT_ELEMENT = document.querySelector('#destination');
 
 const clearUI = ()=>{
     $INPUT_ELEMENT.value = "";
+    $INPUT_ELEMENT.focus();
     Client.setDatePickerValue();
 }
 
