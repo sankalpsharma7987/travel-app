d7b01f8f1c765632a6018f529a552a677b53444e
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-03-23 16:31:52 -0400

    Added logic to remove My Trip heading and removed outline for form elements- Added logic to hide the My Trip heading and also worked on removing outline for the form elements

--- src/client/js/lib/trip/updateSaveTripUI.js
+++ src/client/js/lib/trip/updateSaveTripUI.js
@@ -1,16 +1,28 @@
 const $WEATHER_SAVE_ELEMENT = document.querySelector('.saved-trips');
+const $WEATHER_SAVE_HEADING_ELEMENT = document.querySelector('.saved-trip-heading');
 
 const updateSaveTripUI = (data)=>{
     
-    const tripData = Object.values(data);
     Client.clearSaveTripUI();
+    
     if(!(data.error==='No trips saved'))
     {   
+        const tripData = Object.values(data);
         tripData.forEach(data=>{
             const fragment = Client.createSaveTripElement(data);
             $WEATHER_SAVE_ELEMENT.appendChild(fragment);
         });
 
+        if(!$WEATHER_SAVE_HEADING_ELEMENT.classList.contains('saved-trip-heading-display'))
+        {
+            $WEATHER_SAVE_HEADING_ELEMENT.classList.add('saved-trip-heading-display');
+        }
+        
+
+    }
+
+    else {
+        $WEATHER_SAVE_HEADING_ELEMENT.classList.remove('saved-trip-heading-display');
     }
 
 }
