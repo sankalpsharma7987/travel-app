f86de718f97ad4ca29a76802c25a07eca5af9ede
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-03-28 13:39:51 -0400

    Added condition to verify event target when modal is clicked. Added padding for modal to 32px

--- src/client/js/lib/navigationBurger/hideNavigationModal.js
+++ src/client/js/lib/navigationBurger/hideNavigationModal.js
@@ -1,12 +1,16 @@
 const $NAVIGATION_MODAL_ELEMENT = document.querySelector('.navigation-modal');
 
-const hideNavigationModal = ()=>{
-    $NAVIGATION_MODAL_ELEMENT.classList.add('navigation-modal-slide-out');
-    $NAVIGATION_MODAL_ELEMENT.classList.remove('navigation-modal-slide-in');
-    setTimeout(()=>{
-        $NAVIGATION_MODAL_ELEMENT.classList.remove('navigation-modal-display');
-
-    },600)
+const hideNavigationModal = (e)=>{
+    console.log(e.target.nodeName);
+    if(e.target.nodeName!=='A')
+    {
+        $NAVIGATION_MODAL_ELEMENT.classList.add('navigation-modal-slide-out');
+        $NAVIGATION_MODAL_ELEMENT.classList.remove('navigation-modal-slide-in');
+        setTimeout(()=>{
+            $NAVIGATION_MODAL_ELEMENT.classList.remove('navigation-modal-display');
+    
+        },600)
+    }
 }
 
 $NAVIGATION_MODAL_ELEMENT.addEventListener('click',hideNavigationModal);
