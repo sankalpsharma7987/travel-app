a05f08439a0d289a0c4a42fe0de4a2e5ce8123fb
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-04-07 23:16:25 -0400

    Added footer styling and media queries. Also added photo credits in the footer section

--- src/client/index.js
+++ src/client/index.js
@@ -57,13 +57,14 @@ import './styles/saved-trip-section/saved-trip-section.scss';
 import './styles/user-section/user-section.scss';
 
 
-import './styles/footer-section.scss';
+import './styles/footer-section/footer-section.scss';
 import './styles/image-modal/image-modal.scss';
 import './styles/navigation-section/navigation-section.scss';
 
 //Media Queries
 import './styles/base/media-queries-base.scss';
 import './styles/form-section/media-queries-form-section.scss';
+import './styles/footer-section/media-queries-footer-section.scss';
 import './styles/header-section/media-queries-header-section.scss';
 import './styles/location-section/media-queries-location-section.scss';
 import './styles/navigation-section/media-queries-navigation-section.scss';
